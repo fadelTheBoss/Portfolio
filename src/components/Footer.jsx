9983b16ada3919fe6c028e0a4f957175a405af76
fallou <seyepro930@gmail.com>
2023-10-12 17:57:24 +0200

    upload social media

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -22,29 +22,55 @@ const Footer = () => {
           <a href="#contact" className="block hover:text-gray-400 py-2 px-4">
             Contact
           </a>
-          <a href="#testimonials" className="block hover:text-gray-400 py-2 px-4">
+          <a
+            href="#testimonials"
+            className="block hover:text-gray-400 py-2 px-4"
+          >
             Testimonials
           </a>
         </div>
 
         <div className="flex item-center gap-4 ml-4 ">
+          <a href="https://www.facebook.com/people/Benfadel-Seye/pfbid02kvtUu2k5b6oMbreMVqy1VGFvHBAeoXbewcWhLVDsCKMdnVUFg7V1EckUbfnNUrJHl/" target="_blank" rel="noopener noreferrer">
+
           <FaFacebook className={`${styles.socialMedia}`} />
-          <FaInstagram className={`${styles.socialMedia}`} />
-          <FaTwitter className={`${styles.socialMedia}`} />
+          </a>
+
+          <a
+            href="https://www.instagram.com/fallou_099/?igshid=MzMyNGUyNmU2YQ%3D%3D"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaInstagram className={`${styles.socialMedia}`} />
+          </a>
+
+          <a
+            href="https://twitter.com/F4llou_?t=GwKCNeYfMswGgXn63J16fQ&s=09"
+            target=" _blank"
+            rel="noopener noreferrer"
+          >
+            <FaTwitter className={`${styles.socialMedia}`} />
+          </a>
+
           <FaLinkedin className={`${styles.socialMedia}`} />
         </div>
       </div>
       <hr />
-      
+
       <div className="mt-2 flex flex-col md:flex-row justify-between gap-8 ">
         <p className="">Make with Fallou</p>
         <div className="space-x-6">
-            <a href="/" className="hover:text-gray-400">Privacy Police</a>
-            <a href="/" className="hover:text-gray-400">Term of services</a>
-            <a href="/" className="hover:text-gray-400">Coockie Settings</a>
+          <a href="/" className="hover:text-gray-400">
+            Privacy Police
+          </a>
+          <a href="/" className="hover:text-gray-400">
+            Term of services
+          </a>
+          <a href="/" className="hover:text-gray-400">
+            Coockie Settings
+          </a>
         </div>
       </div>
-
     </div>
   );
 };
